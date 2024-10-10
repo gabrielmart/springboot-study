d44585ed2d38645e4c43b81d7ec537d1642bda84
Gabriel Martins <gmartins@mundolivre.digital>
2024-10-10 15:11:35 -0300

    create Todo Entity

---
+++ src/main/java/br/com/martins/todolist/entity/Todo.java
@@ -0,0 +1,56 @@
+package br.com.martins.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String descricao;
+    private Boolean realizado;
+    private Integer priodade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(Boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public Integer getPriodade() {
+        return priodade;
+    }
+
+    public void setPriodade(Integer priodade) {
+        this.priodade = priodade;
+    }
+
+}
