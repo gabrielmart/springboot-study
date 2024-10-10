1546ac0f50191661cf6225ec5f114adf0c148ec1
Gabriel Martins <gmartins@mundolivre.digital>
2024-10-10 15:11:52 -0300

    create Todo Repository

---
+++ src/main/java/br/com/martins/todolist/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package br.com.martins.todolist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.martins.todolist.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
