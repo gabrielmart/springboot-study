175a78832469f484ee3dc7babcf343d95b1a58e0
Gabriel Martins <gmartins@mundolivre.digital>
2024-12-05 10:32:13 -0300

    remoção de import não utilizado

--- src/main/java/br/com/martins/todolist/api/controllers/AuthenticationController.java
+++ src/main/java/br/com/martins/todolist/api/controllers/AuthenticationController.java
@@ -1,7 +1,6 @@
 package br.com.martins.todolist.api.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
